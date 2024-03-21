64a5462180f7b05ddab6b37969f7015bf3c4fe38
ameblood <hernandeznicolas211@gmail.com>
2024-03-21 14:07:10 -0600

    flores

---
+++ inteligencia artificial/ejercicio1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+lista =[[2,3,4],
+        [3,4,5],
+        [3,4,5],
+        [3,4,5]]
+arr=np.array(lista)
+arr.shape
+print(arr,lista,arr.shape)
